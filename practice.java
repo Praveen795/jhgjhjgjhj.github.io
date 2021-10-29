62168c560ff8c419528491a97b8d2ed732eb7651
Praveen795 <93025027+Praveen795@users.noreply.github.com>
2021-10-29 11:55:51 +0530

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,15 @@
+package practics;
+
+public class practice {
+	public static void main(String[] args) {
+		int a=29;
+		if(a%2==0){
+			System.out.print("even number");
+			
+		}
+		
+		else {
+			System.out.print("odd number");
+		}
+	}
+}
