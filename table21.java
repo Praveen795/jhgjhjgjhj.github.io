b870d8ceeef31d7862e363e7b3946db1fe14b309
Praveen795 <93025027+Praveen795@users.noreply.github.com>
2021-10-29 12:56:15 +0530

    Add files via upload

---
+++ table21.java
@@ -0,0 +1,13 @@
+package table21;
+
+public class table21 {
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i+=1 ) {
+			for(int j=1; j<=10; j++) {
+				System.out.print(i*j + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
