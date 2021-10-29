ef362e8fbcfe97471506c988926614833c0cf0cd
Praveen795 <93025027+Praveen795@users.noreply.github.com>
2021-10-29 11:59:14 +0530

    Add files via upload

---
+++ pravinp.java
@@ -0,0 +1,15 @@
+package pravin;
+
+public class pravinp {
+	public static void main (String[] args) {
+		
+		int a=59;
+		
+		if(a>0) {
+			System.out.print("positive number");
+		}
+		else {
+			System.out.print("negative number");
+		}
+	}
+}
