ec34062a106ef81916d4c36747f4b1efffa9a32e
Praveen795 <93025027+Praveen795@users.noreply.github.com>
2021-10-29 12:00:03 +0530

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,25 @@
+package pravin;
+
+public class pravinp {
+	public static void main (String[] args) {
+		int count=0;
+		
+		for(int i=2; i<99; i+=1;) {
+			
+			for(int j=2; j<i; j+=1;) {
+				
+				if(i%j!=0) {
+					count=count+1;
+					
+				}
+				
+			}
+			
+		}
+		if(count==0) {
+			System.out.print(i);
+		}
+		
+		
+	}
+}
